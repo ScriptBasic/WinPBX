9aebf24477d8481bd1d25425bb5b7bedf64c00fb
José Roca <31576089+JoseRoca@users.noreply.github.com>
2017-09-29 07:13:43 +0200

    Add files via upload

---
+++ tools/TLB_501/TLB_501.h
@@ -0,0 +1,146 @@
+// Phoenix generated include file.
+// Used by "TLB_500.rc"
+
+// NOTE: Code may be modified or placed anywhere in a file except
+//       inside a >>PHNX_BEGIN_XXX and >>PHNX_END_XXX pair of tags.
+//       The >>PHNX_XXX_LOAD code block is programmer accessible, and is
+//       only modified when a project is packaged as a custom control.
+//       DO NOT remove the >>PHNX_BEGIN_XXX/>>PHNX_END_XXX tags.
+//       The code generator modifies tagged blocks on every build.
+
+// >>PHNX_BEGIN_DECLARES
+//========================================================================================
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Icon Identifiers
+//
+
+#define IDI_TLB_LOGO_32                      100
+#define IDI_TLB_LOGO_16                      101
+#define IDI_TLB_TYPELIBS_16                  102
+#define IDI_TLB_SPEAKER                      103
+#define IDI_TLB_GRID_16                      104
+#define IDI_TLB_TREEVIEW_16                  105
+#define IDI_TLB_INTERFACES_16                106
+#define IDI_TLB_OPTIONS_16                   107
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Bitmap Identifiers
+//
+
+#define IDB_TLB_EXIT_24                      100
+#define IDB_TLB_INTERFACES_24                101
+#define IDB_TLB_OPEN_16                      102
+#define IDB_TLB_SAVE_24                      103
+#define IDB_TLB_STOP_24                      104
+#define IDB_TLB_EVENTS_24                    106
+#define IDB_TLB_RELOAD_16                    107
+#define IDB_TLB_SAVE_16                      108
+#define IDB_TLB_EXIT_16                      109
+#define IDB_TLB_RELOAD_24                    110
+#define IDB_TLB_HELP_24                      111
+#define IDB_TLB_INTERFACES_D_24              112
+#define IDB_TLB_EVENTS_D_24                  113
+#define IDB_TLB_STOP_D_24                    114
+#define IDB_TLB_SAVE_D_24                    115
+#define IDB_TLB_RELOAD_D_24                  116
+#define IDB_TLB_HELP_D_24                    117
+#define IDB_TLB_EXIT_D_24                    118
+#define IDB_TLB_HELP_16                      119
+#define IDB_TLB_ABOUT_16                     120
+#define IDB_TLB_INTERFACES_16                126
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Menu/Toolbar Identifiers
+//
+
+#define IDR_DIALOG1_MAINMENU                 103
+#define IDM_FILE                             28000
+#define IDM_RELOAD                           28001
+#define IDM_OPEN                             28002
+#define IDM_IDENTIFIERS                      28003
+#define IDM_ENUMERATIONS                     28004
+#define IDM_EXIT                             28005
+#define IDM_MODULES                          28006
+#define IDM_RECORDS                          28007
+#define IDM_EVENTS                           28011
+#define IDM_CODE                             28014
+#define IDM_INTERFACES                       28015
+#define IDM_DISPINTERFACES                   28016
+#define IDM_HELPMENU                         28018
+#define IDM_HELP                             28019
+#define IDM_ABOUT                            28020
+#define IDM_SAVE                             28021
+#define IDM_STOP                             28022
+#define IDM_TOOLBAR_HELP                     28023
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Layout Rules
+//
+
+// WIDGET = CONTROL
+// There are 8 parts: left, top, right, bottom, width, height widget and group.
+// There are 6 metrics: left, top, right, bottom, width and height.
+// There are 4 sides: left, top, right and bottom.
+#define LEFT        0       // Left side of widget
+#define TOP         1       // Top side of widget
+#define RIGHT       2       // Right side of widget
+#define BOTTOM      3       // Bottom side of widget
+#define WIDTH       4       // Width of widget
+#define HEIGHT      5       // Height of widget
+#define WIDGET      6       // Group(one widget)
+#define GROUP       7       // Group(two widgets acting as limits for a range of widgets)
+
+// Action
+#define STRETCH     0       // Metric should be stretched
+#define MOVE        1       // Widget should be moved
+#define VCENTER     2       // Vertically center widget/group
+#define HCENTER     3       // Horizontally center widget/group
+#define LIMITS      5       // Specifies minimum and maximum control size
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Control string and layout tables
+//
+
+// Dialog1 tooltips/layout
+
+#define IDD_DIALOG1                             100
+#define IDC_DIALOG1_STATUSBAR1                  101
+#define IDC_DIALOG1_OPTIONS                     102
+#define IDC_DIALOG1_TOOLBAR1                    104
+#define IDC_DIALOG1_TAB1                        105
+#define IDC_DIALOG1_GRID                        106
+#define IDC_DIALOG1_TREES                       107
+#define IDC_DIALOG1_CODE                        108
+#define IDC_DIALOG1_LISTVIEW1                   109
+#define IDC_DIALOG1_TREEVIEW1                   110
+#define IDC_DIALOG1_SPLITTERBAR1                111
+#define IDC_DIALOG1_TREEVIEW2                   112
+#define IDC_DIALOG1_RICHEDIT_VIEWCODE           113
+#define IDC_DIALOG1_CHECK_USEVTABLEVIEW         114
+#define IDC_DIALOG1_CHECK_USEPREFIX             115
+#define IDC_DIALOG1_CHECK_USEENUMPREFIX         116
+#define IDC_DIALOG1_CHECK_USEHEXNUMBERS         117
+#define IDC_DIALOG1_EDITPREFIX                  121
+#define IDC_DIALOG1_CHECK_USEPRMNAMES           124
+#define IDC_DIALOG1_CHECK_ADDPRMNAMESPREFIX     125
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Control data string table
+//
+
+// Dialog1
+
+// Tab1 tooltips
+#define O_T_IDS_STRING3                      32821
+#define O_T_IDS_STRING4                      32822
+#define O_T_IDS_STRING5                      32823
+#define O_T_IDS_STRING15                     32824
+//========================================================================================
+// >>PHNX_END_DECLARES
